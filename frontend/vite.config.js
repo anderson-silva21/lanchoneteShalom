f6108b650651d4ebff7237a5beb03b94fbb04f7a
anderson-silva21 <andsoares2001@gmail.com>
2026-06-16 23:34:37 -0300

    hotfix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,6 +9,20 @@ export default defineConfig({
     port: 5173,
     allowedHosts: [
       'intranet.lanchoneteshalom',
+      'intranet.lanchoneteshalom.local',
+      '100.82.234.51',
+      '192.168.15.9',
+      'https://containing-hydrogen-involves-quilt.trycloudflare.com'
+    ]
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: [
+      'intranet.lanchoneteshalom',
+      'intranet.lanchoneteshalom.local',
+      '100.82.234.51',
+      '192.168.15.9',
       'https://containing-hydrogen-involves-quilt.trycloudflare.com'
     ]
   }
